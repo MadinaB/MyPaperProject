7a4f25c8e223481eba7a8792daf256c551da8d0b
Madina <madinabektayeva@gmail.com>
2018-01-24 16:57:30 +0900

    First commit

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+	private WeaponScript weapon;
+
+	void Awake()
+	{
+		weapon = GetComponent<WeaponScript>();
+	}
+
+	void Update()
+	{
+		if (weapon != null && weapon.CanAttack)
+		{
+			weapon.Attack(true);
+		}
+	}
+}
---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+	public int hp = 1;
+	public bool isEnemy = true;
+
+	public void Damage(int damageCount)
+	{
+		hp -= damageCount;
+
+		if (hp <= 0)
+		{
+			// Dead!
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		// Is this a shot?
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+		if (shot != null)
+		{
+			if (shot.isEnemyShot != isEnemy)  // Avoid friendly fire
+			{
+				Damage(shot.damage);
+				Destroy(shot.gameObject);    // Remember to always target the game object, otherwise you will just remove the script
+			}
+		}
+	}
+}
